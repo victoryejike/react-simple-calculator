c073843e75cfc773cce0dda994f149b94b9daa3c
Ejike Chinecherem Victory <56884436+victoryejike@users.noreply.github.com>
2021-04-12 23:05:26 +0100

    calculator header

--- src/App.js
+++ src/App.js
@@ -36,8 +36,8 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello World</h1>
+      <h2>Start calculating today and be stress free</h2>
       <CalculatorDisplay calcDisplay={inputValue} answer={answer} />
       <>
         <div>
